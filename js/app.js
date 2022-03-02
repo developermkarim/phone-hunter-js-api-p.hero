973b2252de62b3bdd7c96a31ad8c82945ef31c75
developermkarim <m.karimcu@gmail.com>
2022-03-02 22:44:15 +0600

    final Project completed

--- js/app.js
+++ js/app.js
@@ -1,52 +1,53 @@
-// Loading Spinner
-const loading_spinner = (spinner_style)=>{
-    const spinner = document.getElementById('spinner');
- spinner.style.display  = spinner_style;
+/* Catch loding spinner */
+const lodingSpinner=(condition)=>{
+    document.getElementById("spinner").style.display=condition;
 }
 
-// Click Handler For Search Button
-const handler_btn = () =>{
-    loading_spinner('block');
-    const searchInput = document.getElementById('search-feild');
-    const searchInputValue = searchInput.value;
-    
-    if(searchInputValue == ""){
-        document.getElementById('warning-text').innerText = 'Please Write somthing !!';
-        loading_spinner('none');
+/* click Event hendler for search button*/
+document.getElementById("search-btn").addEventListener("click",function(){
+    lodingSpinner("block");
+    const searchFeild = document.getElementById("search-feild");
+    const searchFeildValue = searchFeild.value;
+    if(searchFeildValue==""){
+        alert("Please Write somthing !!")
+        lodingSpinner("none");
         document.getElementById("notFound").style.display="block";
     }
     else{
-        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchInputValue}`)
-        .then(res => res.json())
-        .then(data => display_all_mobile(data.data))
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchFeildValue.toLowerCase()}`)
+        .then((res)=>res.json())
+        .then((data)=>displayPhoneInfo(data.data))
     }
-}
-const display_all_mobile = (phones) =>{
-    // console.log(phones);
+
+}) 
+
+/*append cards in container */
+const displayPhoneInfo = (phones)=>{
     const container = document.getElementById("container");
-    const showPhoneContent = phones.slice(0,20);
+    const showPhone = phones.slice(0,20);
     
-    showPhoneContent.forEach(per_phone => {
-       // console.log(per_phone);
-       const main_div = document.createElement('div');
-       main_div.innerHTML = `
-       
-       <div class="col">
+    /* clear container */
+    container.textContent="";
+
+    showPhone.forEach(phone=>{
+        const div = document.createElement("div");
+        div.innerHTML=`
+        <div class="col">
         <div class="card h-100 shadow stl">
-        <img src="${per_phone.image}" class="card-img-top px-5 py-2 pt-4" alt="...">
+        <img src="${phone.image}" class="card-img-top px-5 py-2 pt-4" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${per_phone.phone_name}</h5>
-          <p>Brand: ${per_phone.brand}</p>
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p>Brand: ${phone.brand}</p>
           </div>
-          <button onclick="phoneDetails('${per_phone.slug}')" class="btn btn-primary detils w-75 mx-auto mb-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Details</button>
+          <button onclick="PhoneDetils('${phone.slug}')" class="btn btn-primary detils w-75 mx-auto mb-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Details</button>
         </div>
         </div>
+        `
+        container.appendChild(div);
+    })
 
-       `
-       container.appendChild(main_div)
-    });
-
-    // Not mobile Found Area & condision
+    
+    /* not found area & condision*/
     if(container.textContent==""){
         seeAllBtn.style.display="none";
         const searchFeild = document.getElementById("search-feild");
@@ -60,31 +61,30 @@ const display_all_mobile = (phones) =>{
         const seeAllBtn = document.getElementById("seeAllBtn");
         seeAllBtn.style.display="block";
     }
-
-    loading_spinner("none")
-
+    lodingSpinner("none")
 }
 
-
-// call Phone API
-const phoneDetails = () =>{
+/* call phoneApi */
+const PhoneDetils = (id)=>{
     fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
-    .then(res => res.json())
-    .then(data => display_phone_detail(data))
+    .then(res=>res.json())
+    .then(data=>displayDetils(data))
 }
 
-const display_phone_detail = (phone) =>{
+/* display ditils in modal*/
+const displayDetils = (phone)=>{
     const details = document.getElementById("details");
     details.textContent="";
     const title =document.getElementById("staticBackdropLabel");
     title.innerText=`${phone.data.name}`
+    const div = document.createElement("div");
 
-     /* releaseDate condition add */
-     if(phone.data.releaseDate===""){
+    /* releaseDate condition add */
+    if(phone.data.releaseDate===""){
         phone.data.releaseDate="No release date found"
       }
 
-       /* append details in modal */
+    /* append details in modal */
     div.innerHTML=`
     <div class="">
     <div class='d-flex justify-content-center'>
@@ -107,12 +107,43 @@ const display_phone_detail = (phone) =>{
       `
       details.appendChild(div);
 
-        /* others information Error handel. */
-        const titles = document.getElementsByClassName("otherTitle");
-        for(const title of titles){
-          if(phone?.data?.others == undefined){
-              title.innerText="This feature does not exist";
-          }
+      /* others information Error handel. */
+      const titles = document.getElementsByClassName("otherTitle");
+      for(const title of titles){
+        if(phone?.data?.others == undefined){
+            title.innerText="This feature does not exist";
         }
-        
+      }
+}
+
+/* call displayPhoneInfo for all phone */
+document.getElementById("seeAllBtn").addEventListener("click",function(){
+    document.getElementById("seeAllBtn").style.display="none";
+    const searchFeild = document.getElementById("search-feild");
+    const searchFeildValue = searchFeild.value;
+    searchFeild.value="";
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchFeildValue.toLowerCase()}`)
+    .then((res)=>res.json())
+    .then((data)=>displayAllPhoneInfo(data.data))
+})
+const displayAllPhoneInfo = (phones)=>{
+    const container = document.getElementById("container");
+    const showPhone = phones.slice(20,phones.length);
+
+    showPhone.forEach(phone=>{
+        const div = document.createElement("div");
+        div.innerHTML=`
+        <div class="col">
+        <div class="card h-100 shadow stl">
+        <img src="${phone.image}" class="card-img-top px-5 py-2 pt-4" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p>Brand: ${phone.brand}</p>
+          </div>
+          <button onclick="PhoneDetils('${phone.slug}')" class="btn btn-primary detils w-75 mx-auto mb-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Details</button>
+        </div>
+        </div>
+        `
+        container.appendChild(div);
+    })
 }
